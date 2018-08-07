dd2bb9d0d2041ac0f431f54308a00a112b3459a7
shibaoxu <baoxu.shi@newtouch.cn>
2018-08-07 09:57:38 +0800

    增加阿里中央库

--- commons/src/main/java/com/newtouch/labrador/commons/db/EntityWithUUID.java
+++ commons/src/main/java/com/newtouch/labrador/commons/db/EntityWithUUID.java
@@ -55,8 +55,12 @@ public class EntityWithUUID {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         EntityWithUUID that = (EntityWithUUID) o;
 
