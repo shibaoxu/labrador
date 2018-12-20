14d8061dce4b990005054b0303b024dea7e09352
shibaoxu <baoxu.shi@newtouch.cn>
2018-12-20 17:08:55 +0800

    移动测试公用代码到commons-test

--- commons/src/main/java/com/labrador/commons/exception/CustomExceptionHandler.java
+++ commons/src/main/java/com/labrador/commons/exception/CustomExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.labrador.accountservice.api;
+package com.labrador.commons.exception;
 
-import com.labrador.commons.exception.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
