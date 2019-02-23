b34e094a8868cfb8480102503dbb8c4950e708b4
shibaoxu <baoxu.shi@newtouch.cn>
2019-02-23 21:53:51 +0800

    refactoring configserver

--- configservice/src/main/java/com/labrador/configservice/ConfigserviceApplication.java
+++ configservice/src/main/java/com/labrador/configservice/ConfigserviceApplication.java
@@ -2,6 +2,7 @@ package com.labrador.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
